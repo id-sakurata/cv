5b52ceba33a0e8e1d83b07ee8db05928a60d0947
id-sakurata <sakuratacrackid@gmail.com>
2022-09-01 08:23:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil Saya</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<style type="text/css">
+		table tr td {
+			padding-top: 5px;
+		}
+		.bg-primary {
+			background-color: #0EB57B;
+		}
+		.text-primary {
+			color: #0EB57B;
+		}
+		.img-sm {
+			width: auto;
+			height: 150px;
+			margin-top: 10px;
+		}
+	</style>
+</head>
+<body style="background-color: #E7E7E7">
+	<div class="container-fluid">
+		<div class="row p-4">
+			<div class="col-md-3">
+				<div class="border-2 bg-white shadow-lg mb-5">
+					<div class="image" align="center">
+						<img class="img-fluid" src="img/me.jpg">
+					</div>
+					<div class="mt-2 p-3">
+						<table class="font-weight-normal text-dark">
+							<tr>
+								<td class="font-weight-bold">NAMA</td>
+								<td class="pl-2">Rdk Bayu I</td>
+							</tr>
+							<tr>
+								<td class="font-weight-bold">TTL</td>
+								<td class="pl-2">Blitar, 21 XXXXX 1996</td>
+							</tr>
+							<tr>
+								<td class="font-weight-bold">GENDER</td>
+								<td class="pl-2">Laki - Laki</td>
+							</tr>
+							<tr>
+								<td class="font-weight-bold">ALAMAT</td>
+								<td class="pl-2">Ds. Sumberdiren, Kec. Garum, Kab. Blitar.</td>
+							</tr>
+							<tr>
+								<td class="font-weight-bold">HP/WA</td>
+								<td class="pl-2">0856552092xx</td>
+							</tr>
+							<tr>
+								<td class="font-weight-bold">PEKERJAAN</td>
+								<td class="pl-2">Mahasiswa</td>
+							</tr>
+						</table>
+					</div>
+					<div style="height: 20px" class="bg-primary"></div>
+				</div>
+			</div>
+			<div class="col-md-9">
+				<div id="banner">
+					<img class="w-100" src="img/banner.jpg">
+				</div>
+				<div align="center" class="mt-5">
+					<span class="display-4 text-primary">Tentang</span>
+				</div>
+				<div id="tentang" class="mt-4 bg-white shadow-lg">
+					<p class="lead p-3">Saya seseorang yang kurang kerjaan yang hobi mempelajari hal yang berhubungan dengan komputer,
+						seperti mempelajari pemrograman, editing foto, editing video, editing audio dan music compossing.
+						Saya tidak memiliki bakat apapun, hanya saja bermodalkan usaha.
+						Dan tidak ada hal yang istimewa di dalam hidup saya.<br>
+						<i>Motto: </i>"Usaha yang keras belum tentu mendapatkan hasil yang maksimal, kau hanya perlu mengulangi usahamu
+						hingga kau mendapatkannya."
+					</p>
+				</div>
+				<hr>
+				<div align="center" class="mt-5">
+					<span class="display-4 text-primary">Kemampuan</span>
+				</div>
+				<div class="row mt-4 bg-white rounded-top p-3 shadow-lg">
+					<div class="col-md-4">
+						<div align="center" class="m-2">
+							<h5 class="text-dark">Web Design</h5>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">HTML</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">CSS</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">JAVASCRIPT</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">BOOTSTRAP</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 5%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">LARAVEL FRAMEWORK</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 5%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div style="height: 20px" class="bg-warning mb-3"></div>
+					</div>
+					<div class="col-md-4">
+						<div align="center" class="m-2">
+							<h5 class="text-dark">Pemrograman</h5>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">PHP</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">JAVA</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">C#</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">DELPHI 7</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Visual Basic 6</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div style="height: 20px" class="bg-success mb-3"></div>
+					</div>
+					<div class="col-md-4">
+						<div align="center" class="m-2">
+							<h5 class="text-dark">Lain - Lain</h5>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Editing Photo</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Editing Video</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Editng Audio</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Music Composser</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="text-dark">Dll.</label>
+							<div class="progress">
+								<div id="progressbar" class="progress-bar" style="width: 10%" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+						</div>
+						<div style="height: 20px" class="bg-danger mb-3"></div>
+					</div>
+				</div>
+				<hr>
+				<div align="center" class="mt-5">
+					<span class="display-4 text-primary">Education</span>
+				</div>
+				<div id="education" class="mt-4 bg-white shadow-lg">
+					<div class="row p-3" align="center">
+						<div class="col-md-3">
+							<h5>Th. 2003 - 2009</h5>
+							<div class="image">
+								<img class="img-sm" src="img/sd.png">
+							</div>
+							<span class="lead">SDN GARUM 3</span><br><br>
+						</div>
+						<div class="col-md-3">
+							<h5>Th. 2009 - 2012</h5>
+							<div class="image">
+								<img class="img-sm" src="img/smp.png">
+							</div>
+							<span class="lead">SMPN 6 BLITAR</span><br><br>
+						</div>
+						<div class="col-md-3">
+							<h5>Th. 2012 - 2015</h5>
+							<div class="image">
+								<img class="img-sm" src="img/smk.png">
+							</div>
+							<span class="lead">SMKN 1 BLITAR</span><br>
+							<i>Teknik Elektronika Industri</i><br><br>
+						</div>
+						<div class="col-md-3">
+							<h5>Th. 2016 - 2020</h5>
+							<div class="image">
+								<img class="img-sm" src="img/unisba.png">
+							</div>
+							<span class="lead">UNISBA BLITAR</span><br>
+							<i>Teknik Informatika</i><br><br>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div align="center" class="mt-5">
+					<span class="display-4 text-primary">Project</span>
+				</div>
+				<div class="row text-dark mt-4 bg-white p-3 shadow-lg">
+					<div class="col-md-6">
+						<div align="center">
+							<h5>Finish</h5>
+						</div>
+						<div class="list">
+							<ul>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div align="center">
+							<h5>On-Going</h5>
+						</div>
+						<div class="list">
+							<ul>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+								<li>Belum ada.</li>
+							</ul>
+						</div>
+					</div>
+					<hr>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
